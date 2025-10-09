6025ceb5840b9038ccb96042b74f70cc1205acd3
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-10-09 17:30:15 +0530

    Add files via upload

---
+++ keypragramm.py
@@ -0,0 +1,18 @@
+for x in range (155):
+    if x % 20==0:
+        print("twist")
+
+    elif x % 15==0:
+        print("bang")
+
+    elif x % 5==0:
+        print("fizz")
+
+    elif x % 3==0:
+        print("buzz")
+
+    elif x % 13==0:
+        print("awa")
+
+    else:
+        print(x)
---
+++ keywords.py
@@ -0,0 +1,6 @@
+a=input("enter a word;")
+for i in a:
+    if (i=='a'):
+        print("a is found")
+    else:
+        print("print a not found")
