5fd7e3d01bb12ebce1660cfcfa4cea6ae1929be6
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-10-19 14:35:45 +0530

    Add files via upload

---
+++ tples.py
@@ -0,0 +1,22 @@
+tuplex=("tuple",False,3.2,1)
+print(tuplex)
+
+tuplex=(4,6,2,8,3,1)
+print(tuplex)
+
+tuplex=tuplex + (0,)
+print(tuplex)
+
+tuplex=(50,10,60,70,50)
+print(tuplex.count(50))
+
+tuplex=(2,4,1,5,4,6,2,8,6,7)
+
+_slice=tuplex[3:5]
+print(_slice)
+
+_slice=tuplex[:6]
+print(_slice)
+
+
+
