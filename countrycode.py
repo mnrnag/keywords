cc7db577c27d5de1bec744afbf12e5dcf4d4605a
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-10-21 11:18:54 +0530

    Add files via upload

---
+++ countrycode.py
@@ -0,0 +1,45 @@
+SPCMcountry_code={'India':'0091',
+                  'Australia':'0025',
+                  'Nepal':'00977',
+                  'America':'001',
+                  'China':'0086',
+                  'Japan':'0081',
+                  'Russia':'007',
+                  'Mexico':'0052',
+                  'Italy':'0039',
+                  'Greece':'0030',
+                  'France':'0034'
+                  }
+
+print("Country code for India-")
+print(SPCMcountry_code. get('India','not found'))
+
+print("Country code for Australia-")
+print(SPCMcountry_code. get('Austrilia','not found'))
+
+print("Country code for Nepal-")
+print(SPCMcountry_code. get('Nepal','not found'))
+
+print("Country code for America-")
+print(SPCMcountry_code. get('America','not found'))
+
+print("Country code for China-")
+print(SPCMcountry_code. get('China','not found'))
+
+print("Country code for Japan-")
+print(SPCMcountry_code. get('Japan','not found'))
+
+print("Country code for Russia-")
+print(SPCMcountry_code. get('Russia','not found'))
+
+print("Country code for Mexico-")
+print(SPCMcountry_code. get('Mexico','not found'))
+
+print("Country code for Italy-")
+print(SPCMcountry_code. get('Italy','not found'))
+
+print("Country code for Greece-")
+print(SPCMcountry_code. get('Greece','not found'))
+
+print("Country code for France-")
+print(SPCMcountry_code. get('France','not found'))
