661bbc29545bd13a1ec7db981e0d3c0240e8eec5
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-10-24 17:35:06 +0530

    Add files via upload

---
+++ intersection.py
@@ -0,0 +1,17 @@
+setx={"red","blue","orange","violet","white","black"}
+sety={"red","orange","violet","purple","cyan"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\n intersection of two is said to be:")
+setz=setx.intersection(sety)
+print(setz)
+
+setx={"red","blue","orange","violet","white","black"}
+sety={"red","orange","violet","purple","cyan"}
+print("Original set elements:")
+print(setx)
+print(sety)
+print("\n union of two is said to be:")
+setz=setx.union(sety)
+print(setz)
