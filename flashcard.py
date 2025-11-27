5c0bcba092d6e8d258ef003cc69e5cca1cb2c1f0
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-11-27 17:48:48 +0530

    Add files via upload

---
+++ flashcard.py
@@ -0,0 +1,26 @@
+class flashcard:
+    def __init__(self,word,meaning):
+        self.word=word
+        self.meaning=meaning
+    def __str__(self):
+        return self.word+'('+self.meaning+')'
+    
+flash=[]
+print("welcome to flashcard aplication")
+
+while(True):
+    word=input("enter the name you want to add to flashcard:")
+    meaning=input("enter the meaning of the word:")
+    flash.append(flashcard(word,meaning))
+    option=int(input("enter 0, if you want to add another flahcard otherwise enter 1:"))
+
+    if (option):
+        break
+
+print("\n yourflascards")
+for i in flash:
+    print(">",i)
+
+
+
+     
