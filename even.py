33388e0697a0380b30b367fb760dd56cf6e5f0d8
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-12-18 17:29:27 +0530

    Add files via upload

---
+++ even.py
@@ -0,0 +1,10 @@
+number=int(input("enter your number"))
+
+if number>0:
+    print("it is a positive number")
+
+elif number<0:
+    print("it is a negative number")
+
+else:
+    print("it is zero")
