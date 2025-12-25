e633f2d1a4b7df6d16c185465b0bf4a9f2918bf9
mnrnag <65539019+mnrnag@users.noreply.github.com>
2025-12-25 13:55:23 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,43 @@
+from tkinter import *
+
+root=Tk()
+
+root.title('Login pad')
+root.geometry('400x400')
+
+frame=Frame(master=root, height=200, width=360, bg='blue')
+
+
+lbl1=Label(frame,text="Full name", bg="red", fg='white', width=12)
+lbl2=Label(frame,text="Email Id", bg="green", fg='white', width=12)
+lbl3=Label(frame,text="Enter password", bg="red", fg='white', width=12)
+
+name_entry=Entry(frame)
+email_entry=Entry(frame)
+pass_entry=Entry(frame, show="*")
+
+def display():
+    name= name_entry.get()
+    greet="Hey"+name
+    message= "\n congratulations for ypur new account!"
+    textbox.insert(End,greet)
+    textbox.insert(End,message)
+
+
+textbox=Text(bg="#bebebe",fg="black")
+
+btn=Button( text="create new account", commnand=display, bg="red")
+
+frame.place(x=20,y=0)
+lbl1.place(x=20,y=20)
+name_entry.place(x=150,y=20)
+lbl2.place(x=20,y=80)
+email_entry.place(x=150,y=80)
+lbl3.place(x=20,y=140)
+pass_entry.place(x=150,y=140)
+btn.place(x=130,y=210)
+textbox.place(y=250)
+
+root.mainloop()
+
+
